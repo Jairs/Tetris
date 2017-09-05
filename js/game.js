ac6f622ca912431df919c5d02c46f35a22bcb54a
Jaris <wusanyoujian@126.com>
2017-09-05 15:35:08 +0800

    define game.js

--- js/game.js
+++ js/game.js
@@ -3,4 +3,58 @@ var Game = function() {
 	var gameDiv;
 	var nextDiv;
 	//游戏矩阵
+	var gameData = [
+		[0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0]
+	];
+	//divs
+	var nextDivs = [];
+	var gameDivs = [];
+	//初始化div
+	var initDiv = function(container,data,divs){
+		for (var i = 0; i < data.length; i++) {
+			var div = [];
+			for (var j = 0; j < data[0].length; j++) {
+				var newNode = document.createElement('div');
+				newNode.className = 'none';
+				newNode.style.top = (i*20) + 'px';
+				newNode.style.left = (j*20) +'px';
+				container.getElementById('game').appendChild(newNode);
+				div.push(newNode);
+			}
+			divs.push(div);
+		}
+	}
+	//刷新div
+	var refreshDiv = function(data,divs){
+		for (var i = 0; i < data.length; i++) {
+			for (var j = 0; j < data[0].length; j++) {
+				if(data[i][j]==0){
+					divs[i][j].className = 'none';
+				}else if (gameData[i][j]==1) {
+					divs[i][j].className = 'done';
+				}else if (gameData[i][j]==2) {
+					divs[i][j].className = 'current';
+				}
+			}
+		}
+	}
 }
